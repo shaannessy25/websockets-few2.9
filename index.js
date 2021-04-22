03e57b3165172748437ab267747b10c7de7653d0
shaannessy25 <shaan.hurley25@gmail.com>
2021-04-22 16:19:43 -0700

    finished challenge 1

---
+++ index.js
@@ -0,0 +1,50 @@
+// Get references to DOM elements
+const sendBtn = document.querySelector('#send')
+const messages = document.querySelector('#messages')
+const messageInput = document.querySelector('#message-input')
+
+let ws
+
+// Display messages from the websocket
+function showMessage(message) {
+    messages.innerHTML += `${message}\n\n` // display the message
+    messages.scrollTop = messages.scrollHeight // scroll to the top
+    messageInput.value = '' // clear the input field
+  }
+  
+
+  function init() {
+    // Clean up before restarting a websocket connection
+    if (ws) {
+      ws.onerror = ws.onopen = ws.onclose = null;
+      ws.close();
+    }
+  
+    // Make a new Websocket
+    ws = new WebSocket('ws://localhost:6969')
+  
+    // Handle the connection when it opens
+    ws.onopen = () => console.log('!Connection opened!')
+  
+    // handle a message event
+    ws.onmessage = (e) => showMessage(e.data)
+    
+    // Handle a close event
+    ws.onclose = () => ws = null
+  
+  }
+  
+  // Handle button clicks
+  sendBtn.onclick = function () {
+    // Send a message
+    if (!ws) {
+      showMessage("No WebSocket connection :(");
+      return;
+    }
+  
+    ws.send(messageInput.value);
+    showMessage(messageInput.value);
+  }
+  
+  init();
+  
